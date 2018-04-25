521d2025504f89766105b47709f222f5ea24ab26
wuchen <wuchen@com.cn>
2018-04-25 10:58:11 +0800

    增加类

---
+++ src/main/java/zhth/bom/management/bom/util/change/WayConst.java
@@ -0,0 +1,16 @@
+package zhth.bom.management.bom.util.change;
+
+public class WayConst {
+
+    /**
+     * 使用最大方法切割
+     */
+    public static final int MATCHPART = 0;
+
+
+    /**
+     * 使用最小方法切割
+     */
+    public static final int MINMATERIA = 1;
+
+}
